dc5f11a49dba7d1e26c46394e19703501f0b200e
Caio Almeida <caioreix@outlook.com>
2022-11-22 22:55:38 -0300

    Update prefabsystem

--- VRising/Systems/PrefabCollectionSystem.cs
+++ VRising/Systems/PrefabCollectionSystem.cs
@@ -1,15 +1,21 @@
 namespace Utils.VRising.Systems;
 
-public static class PrefabCollectionSystem
-{
-    public static ProjectM.PrefabCollectionSystem Get()
-    {
+public static class PrefabCollectionSystem {
+    public static ProjectM.PrefabCollectionSystem Get() {
         return Entities.World.world.GetExistingSystem<ProjectM.PrefabCollectionSystem>();
     }
 
-    public static string GetPrefabName(ProjectM.PrefabGUID hashCode)
-    {
-        var prefabCollectionSystem = Get();
-        return prefabCollectionSystem.PrefabNameLookupMap.GetValueOrDefault(hashCode, "NoPrefabName").ToString();
+    public static string GetPrefabName(ProjectM.PrefabGUID hashCode) {
+        var pcs = Get();
+        string name = "Nonexistent";
+        if (hashCode.GuidHash == 0) {
+            return name;
+        }
+        try {
+            name = pcs.PrefabNameLookupMap[hashCode].ToString();
+        } catch {
+            name = "NoPrefabName";
+        }
+        return name;
     }
 }
