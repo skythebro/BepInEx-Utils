4a762dca1eed4fa60fedd18682ed2f71ab079b2f
Caio Almeida <caioreix@outlook.com>
2023-06-18 18:42:46 -0300

    Remove Vrising utils

--- VRising/Systems/ServerScriptMapper.cs
+++
@@ -1,7 +0,0 @@
-namespace Utils.VRising.Systems;
-
-public static class ServerScriptMapper {
-    public static ProjectM.Scripting.ServerScriptMapper Get() {
-        return Entities.World.world.GetExistingSystem<ProjectM.Scripting.ServerScriptMapper>();
-    }
-}
